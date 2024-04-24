8987f56f7c622a90ce697c964b893822d7cfcfa7
niravcenation <76927258+niravcenation@users.noreply.github.com>
2024-04-24 13:58:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ var randomNumber2 = Math.random();
 randomNumber2 = randomNumber2 * 6;
 randomNumber2 = Math.floor(randomNumber2) + 1;
 document.getElementsByClassName("img2")[0].src = "./dice" + randomNumber2 + ".png";
+
+if (randomNumber1 > randomNumber2) {
+    document.getElementsByTagName("h1")[0].innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins!🚩";
+} else {
+    document.getElementsByTagName("h1")[0].innerHTML = "Draw!";
+}
