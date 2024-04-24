9b056f38690e1d75094d6ee7b629bcb9e6124c93
niravcenation <76927258+niravcenation@users.noreply.github.com>
2024-04-24 14:25:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var randomNumber1 = Math.random();
 randomNumber1 = randomNumber1 * 6;
 randomNumber1 = Math.floor(randomNumber1) + 1;
-document.getElementsByClassName("img1")[0].src = "./dice" + randomNumber1 + ".png";
+randomImage1 = "./dice" + randomNumber1 + ".png";
+var image1 = document.querySelector(".img1");
+image1.setAttribute("src", randomImage1);
 
 var randomNumber2 = Math.random();
 randomNumber2 = randomNumber2 * 6;
 randomNumber2 = Math.floor(randomNumber2) + 1;
-document.getElementsByClassName("img2")[0].src = "./dice" + randomNumber2 + ".png";
+randomImage2 = "./dice" + randomNumber2 + ".png";
+var image2 = document.querySelector(".img2");
+image2.setAttribute("src", randomImage2);
 
 if (randomNumber1 > randomNumber2) {
-    document.getElementsByTagName("h1")[0].innerHTML = "🚩Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
-    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins!🚩";
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 } else {
-    document.getElementsByTagName("h1")[0].innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "Draw!";
 }
