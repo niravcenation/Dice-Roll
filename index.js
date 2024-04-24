017777aef11ca2efbdba421a976a0414250e0095
niravcenation <76927258+niravcenation@users.noreply.github.com>
2024-04-24 13:42:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+document.getElementsByClassName("img1")[0].src = "./dice" + randomNumber1 + ".png";
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+document.getElementsByClassName("img2")[0].src = "./dice" + randomNumber2 + ".png";
